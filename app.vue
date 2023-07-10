7f2c4aac8a2642dbe237355309127a4acd4b8818
AntMam <Anthony.mameli@gmail.com>
2023-07-10 12:30:20 -0400

    2 files modified

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <script setup>
 const count = ref(0);
+const config = useRuntimeConfig();
+const response = await $fetch('/auth/login', {
+  method: 'POST',
+  baseURL: config.public.API_BASE_URL,
+  credentials: 'include',
+  body: {
+    email: config.EMAIL,
+    password: config.PASSWORD,
+  },
+});
 </script>
 
 <template>
