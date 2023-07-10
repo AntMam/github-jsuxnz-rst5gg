8cacb83e8e4cb37ee8b97176c2855706e77edf4f
AntMam <Anthony.mameli@gmail.com>
2023-07-10 12:31:15 -0400

    1 file modified

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  runtimeConfig: {
+    public: {
+      API_BASE_URL: process.env.API_BASE_URL,
+    },
+    EMAIL: process.env.EMAIL,
+    PASSWORD: process.env.PASSWORD,
+  },
+});
